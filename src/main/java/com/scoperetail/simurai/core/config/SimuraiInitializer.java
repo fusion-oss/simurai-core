c8a2b444514736ba9c6d3492cf956dc4c0637e29
ravi-zadpide <ravi.zadpide@scoperetail.com>
2022-06-24 18:49:39 +0530

    Added basic configuration

--- src/main/java/com/scoperetail/simurai/core/config/SimuraiInitializer.java
+++ src/main/java/com/scoperetail/simurai/core/config/SimuraiInitializer.java
@@ -26,15 +26,67 @@ package com.scoperetail.simurai.core.config;
  * =====
  */
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.zeroturnaround.zip.ZipUtil;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class SimuraiInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
+  private final SimuraiConfig simuraiConfig;
+  private final ResourceLoader resourceLoader;
+  private final String resourceDirectory;
+
+  public SimuraiInitializer(
+      final SimuraiConfig simuraiConfig,
+      final ResourceLoader resourceLoader,
+      @Value("${RESOURCE_DIRECTORY}") final String resourceDirectory) {
+    this.simuraiConfig = simuraiConfig;
+    this.resourceLoader = resourceLoader;
+    this.resourceDirectory = resourceDirectory;
+  }
 
   @Override
   public void onApplicationEvent(final ContextRefreshedEvent event) {
-    //TODO : Implementation goes here...
+    try {
+      simuraiConfig.setResourceDirectory(resourceDirectory);
+      downloadResources();
+    } catch (final Exception e) {
+      log.error("Exception occured during initialization: {}", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void downloadResources() throws IOException {
+    final String resourceURL = simuraiConfig.getResourceURL();
+    if (StringUtils.isNotBlank(resourceURL)) {
+      final Resource resource = resourceLoader.getResource(simuraiConfig.getResourceURL());
+      log.info("Downloaded resources from URL: {}", resourceURL);
+      final File resourceDir = getResourceDir();
+      ZipUtil.unpack(resource.getInputStream(), resourceDir);
+    } else {
+      log.warn("Resource URL is not specified, falling back to local resource directory");
+    }
+  }
+
+  private File getResourceDir() throws IOException {
+    final File file = new File(Paths.get(resourceDirectory).toString());
+    if (file.exists()) {
+      FileUtils.cleanDirectory(file);
+    } else {
+      FileUtils.forceMkdir(file);
+    }
+    return file;
   }
 }
